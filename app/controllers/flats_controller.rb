59a81f51c16c636f77926d3bb66cc84add2c49c3
John Tweeddale <j.tweeddale@outlook.com>
2021-05-18 10:54:29 +0100

    added edit and update actions for flats

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -20,6 +20,19 @@ class FlatsController < ApplicationController
     @flat = Flat.find(params[:id])
   end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render "edit"
+    end
+  end
+
   def destroy
     @flat = Flat.find(params[:id])
     @flat.destroy
