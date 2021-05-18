9f55ed34546f8b763cfe019937e4d49d137b0437
John Tweeddale <j.tweeddale@outlook.com>
2021-05-18 08:38:57 +0100

    added flats route resources and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory.',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Lovely Big Second Floor Flat',
+  address: '25 Hamlet Street London SE1 4TQ',
+  description: 'Great views over the Thames make this a great flat for relaxation and it has great proximity to transport links.',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Cramped and Mouldy Basement Flat',
+  address: '82 Yam Lane London W2 A11',
+  description: 'This tiny flat really is not in a good condition. But when you are desparate, maybe you will rent anything?',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Very Average Penthouse Flat',
+  address: '55 Fife Street London SE27 5RR',
+  description: 'This decent sized penthouse flat has great views over the South East, but otherwise it is very average. Might be worth checking out?',
+  price_per_night: 75,
+  number_of_guests: 3
+)
